1fa9815900982dd1a92b9cadde99be758c9d5f10
Nick Woronekin <nickwor@gmail.com>
2018-08-04 10:23:53 -0400

    Fixed issue #1.

--- src/TppkTool/create.cmd
+++ src/TppkTool/create.cmd
@@ -1,11 +1,11 @@
 @echo off
 
-if [%1] == [] goto :eof
+if "%~1" == "" goto :eof
 
 :enteroutput
 set /p output=Name of the TPPK archive to create: 
 
-if [%output%] == [] (
+if "%output%" == "" (
     echo Name cannot be blank.
     goto :enteroutput
 ) else (
